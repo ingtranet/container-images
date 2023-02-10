0ece4015452a17a180d814e350bb6ae1e19c0e32
cookieshake <cookieshake.dev@gmail.com>
2023-02-10 21:26:26 +0900

    build kyuubi

---
+++ kyuubi/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://dlcdn.apache.org/incubator/kyuubi/kyuubi-$KYUUBI_VERSION-incubating/apache-kyuubi-$KYUUBI_VERSION-incubating-bin.tgz
+tar zxvf apache-kyuubi-$KYUUBI_VERSION-incubating-bin.tgz
+./apache-kyuubi-$KYUUBI_VERSION-incubating-bin/bin/docker-image-tool.sh \
+    -r ingtranet-kyuubi -t latest -S /opt/spark -b BASE_IMAGE=$SPARK_IMAGE:$SPARK_TAG build
