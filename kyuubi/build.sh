1aab547d1537995d95aa5a6d2906cb6d0f3b44ec
cookieshake <cookieshake.dev@gmail.com>
2023-02-11 02:29:23 +0900

    change image repository

--- kyuubi/build.sh
+++ kyuubi/build.sh
@@ -3,4 +3,4 @@ set -ex
 wget https://archive.apache.org/dist/incubator/kyuubi/kyuubi-$KYUUBI_VERSION-incubating/apache-kyuubi-$KYUUBI_VERSION-incubating-bin.tgz
 tar zxvf apache-kyuubi-$KYUUBI_VERSION-incubating-bin.tgz
 ./apache-kyuubi-$KYUUBI_VERSION-incubating-bin/bin/docker-image-tool.sh \
-    -r ingtranet-kyuubi -t latest -S /opt/spark -b BASE_IMAGE=$SPARK_IMAGE:$SPARK_TAG build
+    -r ingtranet -t latest -S /opt/spark -b BASE_IMAGE=$SPARK_IMAGE:$SPARK_TAG build
