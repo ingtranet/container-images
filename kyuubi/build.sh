cc738f0fc3487f1b5bf4a91f4d895613d648e2ab
cookieshake <cookieshake.dev@gmail.com>
2023-02-11 01:41:59 +0900

    change kyuubi download url

--- kyuubi/build.sh
+++ kyuubi/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget https://dlcdn.apache.org/incubator/kyuubi/kyuubi-$KYUUBI_VERSION-incubating/apache-kyuubi-$KYUUBI_VERSION-incubating-bin.tgz
+set -ex
+wget https://archive.apache.org/dist/incubator/kyuubi/kyuubi-$KYUUBI_VERSION-incubating/apache-kyuubi-$KYUUBI_VERSION-incubating-bin.tgz
 tar zxvf apache-kyuubi-$KYUUBI_VERSION-incubating-bin.tgz
 ./apache-kyuubi-$KYUUBI_VERSION-incubating-bin/bin/docker-image-tool.sh \
     -r ingtranet-kyuubi -t latest -S /opt/spark -b BASE_IMAGE=$SPARK_IMAGE:$SPARK_TAG build
