88909a11790bae552d224fd66e0dff6083962ef5
cookieshake <cookieshake.dev@gmail.com>
2023-05-05 23:32:39 +0900

    [airflow][caddy][kyuubi][opensearch-dashboards][querybook][spark] add arm64 images

--- kyuubi/build.sh
+++ kyuubi/build.sh
@@ -3,4 +3,4 @@ set -ex
 wget https://dlcdn.apache.org/kyuubi/kyuubi-$KYUUBI_VERSION/apache-kyuubi-$KYUUBI_VERSION-bin.tgz
 tar zxvf apache-kyuubi-$KYUUBI_VERSION-bin.tgz
 ./apache-kyuubi-$KYUUBI_VERSION-bin/bin/docker-image-tool.sh \
-    -r ingtranet -t latest -S /opt/spark -b BASE_IMAGE=$SPARK_IMAGE:$SPARK_TAG build
+    -r ingtranet -t latest -S /opt/spark -b BASE_IMAGE=$SPARK_IMAGE:$SPARK_TAG -X build
