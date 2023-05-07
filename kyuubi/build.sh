b9b178bc659fba88ab8375e84b2b17f009a23bea
cookieshake <cookieshake.dev@gmail.com>
2023-05-08 01:08:30 +0900

    change kyuubi tag method

--- kyuubi/build.sh
+++ kyuubi/build.sh
@@ -3,4 +3,4 @@ set -ex
 wget https://dlcdn.apache.org/kyuubi/kyuubi-$KYUUBI_VERSION/apache-kyuubi-$KYUUBI_VERSION-bin.tgz
 tar zxvf apache-kyuubi-$KYUUBI_VERSION-bin.tgz
 ./apache-kyuubi-$KYUUBI_VERSION-bin/bin/docker-image-tool.sh \
-    -r ingtranet -t latest -S /opt/spark -b BASE_IMAGE=$SPARK_IMAGE:$SPARK_TAG -X build
+    -r ingtranet -t $KYUUBI_VERSION-spark$SPARK_TAG -S /opt/spark -b BASE_IMAGE=$SPARK_IMAGE:$SPARK_TAG -X build
